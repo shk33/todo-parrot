43421a15e60d5bb6a3b6568dbf85e7f67dc2899c
Miguel Coronel <m.coronel.seg@gmail.com>
2015-08-25 00:49:17 -0500

    Add master layout

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Welcome to TODOParrot</title>
-  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-</head>
-  <body>
-    <h1>Welcome to TODOParrot</h1>
-    <p>Welcome to {{$name}} on {{$date}}</p>
-  </body>
-</html>
+@@extends('layouts.master')
+  <h1>Welcome to TODOParrot</h1>
+  <p>Welcome to {{$name}} on {{$date}}</p>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to TODOParrot</title>
+  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+</head>
+  <body>
+    @yield('content')
+  </body>
+</html>
