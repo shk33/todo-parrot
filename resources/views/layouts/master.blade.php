cd69e5d7520772908625292e9fb6062882fde6b3
Miguel Coronel <m.coronel.seg@gmail.com>
2015-08-25 01:21:02 -0500

    Add bootstrap and jquery

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,9 @@
 <head>
   <title>Welcome to TODOParrot</title>
   <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
   <body>
     @yield('content')
