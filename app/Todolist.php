ba8a0225d1c7138bddc117790d036df9e7479012
Miguel Coronel <m.coronel.seg@gmail.com>
2015-08-26 01:19:44 -0500

    Comment a laravel attribute mutator

--- app/Todolist.php
+++ app/Todolist.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todolist extends Model
 {
-    //
+  // public function setPasswordAttribute($password)
+  // {
+  //   $this->attributes['password'] = \Hash::make($password);
+  // }
 }
