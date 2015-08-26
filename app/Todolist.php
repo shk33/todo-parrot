29b92e82ffc122c3e117a07a50c9d789b881e26f
Miguel Coronel <m.coronel.seg@gmail.com>
2015-08-26 01:32:19 -0500

    Add validation rules and filled array to Todolist model

--- app/Todolist.php
+++ app/Todolist.php
@@ -6,6 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todolist extends Model
 {
+  private $rules = [
+    'name' => 'required',
+    'description' => 'required'
+  ];
+
+  protected $fillable = ['name', 'description'];
+
+  function validate()
+  {
+    $v = \Validator::make($this->attributes, $this->rules);
+    if ($v->passes()) return true;
+    $this->errors = $v->messages();
+    return false;
+  }
   // public function setPasswordAttribute($password)
   // {
   //   $this->attributes['password'] = \Hash::make($password);
